5a8d8ea381304909393ca5be670bc1ce1179f922
Cody <dakotalrubin@gmail.com>
2022-09-18 09:56:53 -0700

    Add playSound() and removeTransition() functions to highlight current key press

--- script/script.js
+++ script/script.js
@@ -1,13 +1,29 @@
-// This function waits for a "keydown" event, checks whether the pressed key
-// has an associated audio element, and plays that sound clip if present.
-window.addEventListener("keydown", function(e) {
+// This function plays an audio element for an associated "keydown" event.
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+    // Exits function if key outside expected range is pressed.
     if (!audio) {
-        return; // Exits function if key outside expected range is pressed.
+        return;
     }
 
     audio.currentTime = 0; // Plays sound clip again immediately if re-pressed.
     audio.play();
-});
+    // ".toggle" instead of ".add" fixes permanent transition state if holding key
+    key.classList.toggle("playing");
+}
+
+function removeTransition(e) {
+    // Skips all non-transform event properties.
+    if (e.propertyName !== "transform") {
+        return;
+    }
+
+    // "this" operates on key.addEventListener("transitionend", removeTransition));
+    this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll(".key"); // Creates Nodelist of all keys.
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
